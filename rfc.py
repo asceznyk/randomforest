ebeab4371829c60f615f8e6f65221a3c8d477b5d
asceznyk <asceznyk@gmail.com>
2022-03-19 19:56:01 +0530

    ..

--- rfc.py
+++ rfc.py
@@ -2,28 +2,87 @@ from random import seed, randrange
 from csv import reader
 from math import sqrt
 
-def load_csv(filename):
-    dataset = list()
-    with open(filename, 'r') as file:
-        csv_reader = reader(file)
-        for row in csv_reader:
-            if not row:
-                continue
-            dataset.append(row)
-    return dataset
+def class_values(dataset):
+    return list(set(row[-1] for row in dataset))
 
-def str_column_to_float(dataset, column):
-    for row in dataset: row[column] = float(row[column].strip())
-
-def str_column_to_int(dataset, column):
-    class_values = [row[column] for row in dataset]
-    unique = set(class_values)
-    lookup = dict()
-    for i, value in enumerate(unique):
-        lookup[value] = i
+def attr_split(attr, value, dataset):
+    left, right = [], []
     for row in dataset:
-        row[column] = lookup[row[column]]
-    return lookup
+        if row[attr] < value:
+            left.append(row)
+        else:
+            right.append(row)
+    
+    return left, right
+
+def gini_index(groups, classes):
+    n_instances = float(sum([len(group) for group in groups]))
+    
+    gini = 0.0
+    for group in groups:
+        size = float(len(group))
+        if size == 0.0: continue
+        score = 0.0
+        for c in classes:
+             p = [row[-1] for row in group].count(c) / size
+             score += p*p
+        gini += (1.0 - score) * (size / n_instances)
+    
+    return gini
+
+def count_classify(group):
+    outcomes = [row[-1] for row in group]
+    return max(set(outcomes), key=outcomes.count)
+
+def best_split(dataset, n_features):
+    classes = class_values(dataset)
+    b_attr, b_value, b_score, b_groups = 999, 999, 999, None
+
+    features = []
+    while len(features) < n_features:
+        f = randrange(len(dataset[0])-1) 
+        if f not in features: 
+            features.append(f) 
+    
+    for attr in features:
+        for row in dataset:
+            groups = attr_split(attr, row[attr], dataset)
+            gini = gini_index(groups, classes)
+            if gini < b_score:
+                b_attr, b_value, b_score, b_groups = attr, row[attr], gini, groups
+    
+    return {'attr': b_attr, 'value': b_value, 'groups':b_groups}
+
+def binary_split(node, n_features, min_size, max_depth, depth):
+    left, right = node['groups']
+    del node['groups']
+
+    if not left or not right: 
+        node['left'] = node['right'] = count_classify(left+right)
+        return
+
+    if depth >= max_depth:
+        node['left'], node['right'] = count_classify(left), count_classify(right)
+        return
+
+    if len(left) <= min_size:
+        node['left'] = count_classify(left)
+    else:
+        node['left'] = best_split(left, n_features)
+        binary_split(node['left'], n_features, min_size, max_depth, depth+1)
+
+    if len(right) <= min_size:
+        node['right'] = count_classify(right)
+    else:
+        node['right'] = best_split(right, n_features)
+        binary_split(node['right'], n_features, min_size, max_depth, depth+1)
+
+def build_tree(dataset, n_features,  min_size, max_depth):
+    root = best_split(dataset, n_features)
+    binary_split(root, n_features, min_size, max_depth, 1)
+    return root
+
+
 
 
 
--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 from random import seed
+
+from utils import *
 from rfc import * 
 
 seed(2)
@@ -7,4 +9,7 @@ for i in range(0, len(dataset[0])-1):
     str_column_to_float(dataset, i)
 str_column_to_int(dataset, len(dataset[0])-1)
 
+n_features = len(dataset[0])-1
+tree = build_tree(dataset, n_features, 1, 10)
+print(tree)
 
