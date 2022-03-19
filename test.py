4494757905e0435b82f383dd438ab6e8308b2822
asceznyk <asceznyk@gmail.com>
2022-03-19 16:33:38 +0530

    first

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
+from random import seed
 from rfc import * 
 
+seed(2)
 dataset = load_csv('sonar.csv')
-print(dataset)
-
-
+for i in range(0, len(dataset[0])-1):
+    str_column_to_float(dataset, i)
+str_column_to_int(dataset, len(dataset[0])-1)
 
 
